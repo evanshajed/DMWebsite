35834d047c0da46e4541e026f484d0cdf100c06a
Shajed Evan <evanshajed@gmail.com>
2012-04-22 16:47:31 +0600

    Sprite generated
    Few Small Changes on Basic Frame

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -2,6 +2,19 @@
 	Shajed Evan
 */
 
+WebFontConfig = {
+    google: { families: [ 'Bree+Serif::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
 
 
 
